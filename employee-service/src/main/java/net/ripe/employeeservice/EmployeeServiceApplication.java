9fdb89f3ba5ed13df8b9a1b1d8624b64d343800c
fnodado <francisdahryljava@gmail.com>
2022-11-12 11:57:52 +0800

    added service-registry implementation to track the services and the number of instances to it

--- employee-service/src/main/java/net/ripe/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/ripe/employeeservice/EmployeeServiceApplication.java
@@ -2,6 +2,8 @@ package net.ripe.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +11,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
