579a78eaaeb98ad2cab351f94391adca6387dfa4
fnodado <francisdahryljava@gmail.com>
2022-11-13 11:35:09 +0800

    Added config-server

---
+++ config-server/src/test/java/net/ripe/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ripe.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
